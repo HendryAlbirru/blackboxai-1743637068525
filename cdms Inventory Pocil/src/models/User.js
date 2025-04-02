31fb09e194c8cbb60b5fe322bddfea27b76ebfbb
HendryAlbirru <ddosprofeling@gmail.com>
2025-04-02 19:38:20 -0400

    Add cdms Inventory Pocil/src/models/User.js 1743637089167

---
+++ cdms Inventory Pocil/src/models/User.js
@@ -0,0 +1,80 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+  id_user: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    unique: true // Add unique constraint
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      len: [3, 50]
+    }
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('admin', 'gudang', 'auditor', 'customs'),
+    defaultValue: 'gudang',
+    allowNull: false
+  }
+}, {
+  tableName: 'tbl_users',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: false,
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  },
+  indexes: [
+    {
+      unique: true,
+      fields: ['id_user']
+    },
+    {
+      unique: true,
+      fields: ['username']
+    },
+    {
+      unique: true,
+      fields: ['email']
+    },
+    {
+      fields: ['role']
+    }
+  ]
+});
+
+// Instance method to check password
+User.prototype.validatePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = User;
